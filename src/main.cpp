f6805b68c858b67ee09c954a5deb14350c478d01
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-03-01 00:54:22 +0100

    add osc send receive test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <Arduino.h>
 #include <UIPEthernet.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+
+// Networking / UDP Setup
+EthernetUDP Udp;
+
+// destination address
+IPAddress targetIP(10, 0, 10, 101);
+const unsigned int targetPort = 8000;
+const unsigned int inPort = 8888;
 
 byte mac[] = { 0x54, 0x34, 0x41, 0x30, 0x30, 0x31 };
 
@@ -17,6 +27,13 @@ void printIPAddress()
   Serial.println();
 }
 
+void pingOSCHandler(OSCMessage &msg, int addrOffset) {
+  int inValue = msg.getInt(0);
+
+  Serial.print("osc msg value: ");
+  Serial.println(inValue);
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
@@ -31,6 +48,8 @@ void setup() {
     }
     // print your local IP address:
     printIPAddress();
+
+    Udp.begin(inPort);
 }
 
 // the loop function runs over and over again forever
@@ -48,6 +67,14 @@ void loop() {
 
        //print your local IP address:
        printIPAddress();
+
+       OSCMessage msg("/test");
+       msg.add(100);
+       Udp.beginPacket(targetIP, targetPort);
+       msg.send(Udp); // send the bytes to the SLIP stream
+       Udp.endPacket(); // mark the end of the OSC Packet
+       msg.empty(); // free space occupied by message
+
        break;
 
      case 3:
@@ -68,4 +95,31 @@ void loop() {
        break;
 
    }
+
+   // read incoming udp packets
+   OSCMessage msgIn;
+   int size;
+   int success;
+
+   if( (size = Udp.parsePacket())>0)
+   {
+
+     //while((size = Udp.available()) > 0)
+     while(size--)
+       msgIn.fill(Udp.read());
+
+     // route messages
+     if(!msgIn.hasError()) {
+       msgIn.route("/servo", pingOSCHandler);
+     }
+
+     //finish reading this packet:
+     Udp.flush();
+
+     //restart UDP connection to receive packets from other clients
+     Udp.stop();
+     success = Udp.begin(inPort);
+
+   }
+
 }
