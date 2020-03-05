e9ebaa6854145dbd2d352ebd907d69b437c71efb
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-03-05 20:09:16 +0100

    add servo 0 test

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,46 @@
 
 #include <Arduino.h>
 
+// PCA9685 16-channel PWM servo driver
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+uint8_t servonum = 0;
+
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+
+  pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
+  Serial.print(pulselength); Serial.println(" us per period");
+  pulselength /= 4096;  // 12 bits of resolution
+  Serial.print(pulselength); Serial.println(" us per bit");
+  pulse *= 1000000;  // convert input seconds to us
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
+
 void setup() {
   #ifdef SERIAL_DEBUGING
     Serial.begin(9600);
   #endif
+
+  //initialize servo board
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  delay(10);
+
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -13,8 +49,14 @@ void setup() {
 
 
 void loop() {
-  #ifdef SERIAL_DEBUGING
-    Serial.println("serial test");
-  #endif
-  delay(1000);
+  Serial.println(servonum);
+    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+      pwm.setPWM(servonum, 0, pulselen);
+    }
+
+    delay(500);
+    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+      pwm.setPWM(servonum, 0, pulselen);
+    }
+
 }
