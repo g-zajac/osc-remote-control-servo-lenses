042fef7502039e25ae4617ba0b5346163c4a1e20
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-03-05 19:10:42 +0100

    add serial with conditional compiling and test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
+#define SERIAL_DEBUGING     // comment it out to disable serial debuging, for production i.e.
+
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  #ifdef SERIAL_DEBUGING
+    Serial.begin(9600);
+  #endif
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  // put your main code here, to run repeatedly:
 
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+void loop() {
+  #ifdef SERIAL_DEBUGING
+    Serial.println("serial test");
+  #endif
+  delay(1000);
 }
