a533fbd1977dc8aa7d24fac05d4749807f6d7669
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-04-08 00:31:31 +0200

    add AccelStepper with two diferent motors, test

--- src/main.cpp
+++ src/main.cpp
@@ -42,20 +42,29 @@ int motorStepPin = 5; //digital pin 3
 
 //set up the accelStepper intance
 //the "1" tells it we are using a driver
-AccelStepper stepper(1, motorStepPin, motorDirPin);
+AccelStepper stepper1(1, motorStepPin, motorDirPin);
+AccelStepper stepper2(AccelStepper::FULL4WIRE, 17,19,18,20);
+
 void setup()
 {
   // Change these to suit your stepper if you want
-  stepper.setMaxSpeed(400);
-  stepper.setAcceleration(50);
-  stepper.moveTo(2038);
+  stepper1.setMaxSpeed(400);
+  stepper1.setAcceleration(50);
+  stepper1.moveTo(2038);
+
+  stepper2.setMaxSpeed(400);
+  stepper2.setAcceleration(50);
+  stepper2.moveTo(2038);
 }
 
 void loop()
 {
     // If at the end of travel go to the other end
-    if (stepper.distanceToGo() == 0)
-      stepper.moveTo(-stepper.currentPosition());
-
-    stepper.run();
+    if (stepper1.distanceToGo() == 0)
+      stepper1.moveTo(-stepper1.currentPosition());
+    if (stepper2.distanceToGo() == 0)
+      stepper2.moveTo(-stepper2.currentPosition());
+      
+    stepper1.run();
+    stepper2.run();
 }
