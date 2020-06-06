18b3cc2b0051507f6a6a6dfd4f0a3be89fada10f
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-06-07 00:36:01 +0200

    add simple motor step with AccelStepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,33 @@
 #include <Arduino.h>
-#include "BasicStepperDriver.h"
-
-// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#include <AccelStepper.h>
 
 // 28BYJ-48 motor runs in full step mode, each step corresponds to a rotation of 11.25°.
 // That means there are 32 steps per revolution (360°/11.25° = 32). What this means is that
 // there are actually 32*63.68395 steps per revolution = 2037.8864 ~ 2038 steps!
 
-
-// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
-#define MOTOR_STEPS 32
-// Target RPM for cruise speed
-#define RPM 120*2
-// Acceleration and deceleration values are always in FULL steps / s^2
-#define MOTOR_ACCEL 500
-#define MOTOR_DECEL 250
-
-// Microstepping mode. If you hardwired it to save pins, set to the same value here.
-#define MICROSTEPS 1
-
-#define DIR 4
-#define STEP 5
-#define SLEEP 8 // optional (just delete SLEEP from everywhere if not used)
-
-/*
- * Choose one of the sections below that match your board
- */
-
-// #include "DRV8834.h"
-// #define M0 6
-// #define M1 7
-// DRV8834 stepper(MOTOR_STEPS, DIR, STEP, SLEEP, M0, M1);
-
-
-#include <AccelStepper.h>
-
 // Define a stepper and the pins it will use
 // AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
-int motorDirPin = 4; //digital pin 2     < ===THIS IS A DIRECTION PIN
-int motorStepPin = 5; //digital pin 3
+
+
+// Motor one bipolar, converted 28BYJ-48 with DRV8834 driver
+int motor1DirPin = 4;
+int motor1StepPin = 5;
+
+// Motor two unipolar ULN2003
+int motor2pin1 = 17;
+int motor2pin2 = 18;
+int motor2pin3 = 19;
+int motor2pin4 = 20;
+
+// Motor three, DC pot with 6612 DRIVER
+int motor3pin1 = 15;
+int motor3pin2 = 16;
 
 //set up the accelStepper intance
 //the "1" tells it we are using a driver
-AccelStepper stepper1(1, motorStepPin, motorDirPin);
-AccelStepper stepper2(AccelStepper::FULL4WIRE, 17,19,18,20);
+AccelStepper stepper1(AccelStepper::DRIVER, motor1StepPin, motor1DirPin);
+AccelStepper stepper2(AccelStepper::FULL4WIRE, motor2pin1,motor2pin3,motor2pin2,motor2pin4);
+AccelStepper stepper3(AccelStepper::DRIVER, motor3pin1,motor3pin2);
 
 void setup()
 {
@@ -64,7 +48,7 @@ void loop()
       stepper1.moveTo(-stepper1.currentPosition());
     if (stepper2.distanceToGo() == 0)
       stepper2.moveTo(-stepper2.currentPosition());
-      
+
     stepper1.run();
     stepper2.run();
 }
